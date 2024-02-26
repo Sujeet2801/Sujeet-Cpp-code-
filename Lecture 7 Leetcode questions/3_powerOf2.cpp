1a314674135259abdcc905aa9804bd82ec62e33b
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2024-02-26 08:24:59 +0000

    Lecture 6

---
+++ Lecture 7 Leetcode questions/3_powerOf2.cpp
@@ -0,0 +1,30 @@
+
+// C++ program for the above approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to check if x is power of 2*/
+bool isPowerOfTwo(int n)
+{
+	if (n == 0)
+		return 0;
+        
+	while (n != 1) {
+		if (n % 2 != 0)
+			return 0;
+		n = n / 2;
+	}
+	return 1;
+}
+
+// Driver code
+int main()
+{
+	// Function call
+	isPowerOfTwo(31) ? cout << "Yes\n" : cout << "No\n";
+	isPowerOfTwo(64) ? cout << "Yes\n" : cout << "No\n";
+	return 0;
+}
+
+// This code is contributed by rathbhupendra
