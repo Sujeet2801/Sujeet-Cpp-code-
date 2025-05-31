9c783c6a5d9a032ab6b55f324fe032c25aadb896
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-06-01 01:58:33 +0530

    feat(Array/duplicateNumber): Add function to find the first duplicate in an integer array using unordered_set

--- Lecture 10 Array Leetcode Problem/4_duplicateNumber.cpp
+++ Lecture 10 Array Leetcode Problem/4_duplicateNumber.cpp
@@ -19,6 +19,18 @@ int dupilcateNumber(int arr[], int n)
     return ans;
 }
 
+int findFirstDuplicate(int arr[], int size){
+    unordered_set<int> seen;
+
+    for(int i = 0; i < size; i++) {
+        if(seen.count(arr[i]))
+            return arr[i];
+        seen.insert(arr[i]);
+    }
+
+    return -1;
+}
+
 int main()
 {
     int arr[50] = {4,4,5,1,2,3};
